e7c1babb5b3be9de8864ab2605212cffc9cf8c7e
zak-off-zak <Zakhar_2004@inbox.ru>
2025-02-20 18:28:01 +0100

    Moved collition checker to the Utilities file

--- include/CollisionManager.h
+++
@@ -1,8 +0,0 @@
-#ifndef COLLISION_MANAGER
-#define COLLISION_MANAGER
-
-#include <SFML/Graphics/Rect.hpp>
-
-bool check_collision(const sf::FloatRect &a, const sf::FloatRect &b);
-
-#endif // !COLLISION_MANAGER
--- src/CollisionManager.cpp
+++
@@ -1,5 +0,0 @@
-#include "../include/CollisionManager.h"
-
-bool check_collision(const sf::FloatRect &a, const sf::FloatRect &b) {
-  return !(a.findIntersection(b) == std::nullopt);
-}
