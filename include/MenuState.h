21182eecaa65d5b3dc78eb2dcf1fc3c89fd16882
zak-off-zak <Zakhar_2004@inbox.ru>
2025-02-28 18:05:05 +0100

    MenuState docs added

--- include/MenuState.h
+++ include/MenuState.h
@@ -8,13 +8,48 @@
 #include <SFML/Graphics/Text.hpp>
 #include <SFML/Graphics/Texture.hpp>
 
+/**
+ * @class MenuState
+ * @brief State that handles all of the processes related to the menu
+ *
+ */
 class MenuState : public GameState {
 public:
+  /**
+   * @brief Menu state constructor
+   */
   MenuState();
 
+  /**
+   * @brief Initiates new menu state
+   *
+   * @param game Game game object containing important global informatio about
+   * the game
+   */
   void init(Game *game) override;
+  /**
+   * @brief Handles all of the user input regarding the menu
+   *
+   * @param game Game game object containing important global informatio about
+   * the game
+   * @param event Event that has to be triggered
+   */
   void handle_input(Game *game, const std::optional<sf::Event> &event) override;
+  /**
+   * @brief Updates the internal state of the the menu
+   *
+   * @param game Game game object containing important global informatio about
+   * the game
+   * @param time Time step to calculate the new state of the menu
+   */
   void update(Game *game, float time) override;
+  /**
+   * @brief Renders all of the internal objects of the menu state
+   *
+   * @param game Game game object containing important global informatio about
+   * the game
+   * @param window Window for the objects to be displayed in
+   */
   void render(Game *game, sf::RenderWindow &window) override;
 
 protected:
