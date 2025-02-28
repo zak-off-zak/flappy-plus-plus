01ed60d7d85520d9b756044c8acc707f2051e2e3
zak-off-zak <Zakhar_2004@inbox.ru>
2025-02-28 18:25:53 +0100

    CustomButton docs added

--- include/CustomButton.h
+++ include/CustomButton.h
@@ -10,14 +10,42 @@
 #include <SFML/Window/Event.hpp>
 #include <functional>
 
+/**
+ * @class CustomButton
+ * @brief A custom consisting of a shape, text and an event handler
+ *
+ */
 class CustomButton : public sf::Drawable {
 public:
+  /**
+   * @brief Default constructor of the custom button
+   */
   CustomButton();
+  /**
+   * @brief Copy constructor of the custom button
+   *
+   * @param other [TODO:parameter]
+   */
   CustomButton(const CustomButton &other);
+  /**
+   * @brief Constructor of the custom button
+   *
+   * @param position Position of the custom button
+   * @param size Size of the custom button
+   * @param shape Shape of the custom button
+   * @param color Color of the custon button
+   * @param on_click Function to be triggered when the button is pressed
+   */
   CustomButton(const sf::Vector2f &position, const sf::Vector2f &size,
                const sf::RectangleShape &shape, const sf::Color &color,
                const std::function<void()> &on_click);
 
+  /**
+   * @brief Handles events to determine whether to trigger the on_click function
+   *
+   * @param event Event to be handeled
+   * @param window Window to dertermine the position of the mouse
+   */
   void handle_event(const std::optional<sf::Event> &event,
                     const sf::RenderWindow &window);
 
