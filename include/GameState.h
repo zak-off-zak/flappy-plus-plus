4cc53efbfe78f79a7edc2fd6d71662e02690ce59
zak-off-zak <Zakhar_2004@inbox.ru>
2025-02-28 17:48:05 +0100

    GameState docs added

--- include/GameState.h
+++ include/GameState.h
@@ -6,14 +6,49 @@
 
 class Game;
 
+/**
+ * @class GameState
+ * @brief Class providing the interface for the states managed by the game
+ *
+ */
 class GameState {
 public:
+  /**
+   * @brief Destructor of the state
+   */
   virtual ~GameState() {}
 
+  /**
+   * @brief Initiates new state
+   *
+   * @param game Game game object containing important global informatio about
+   * the game
+   */
   virtual void init(Game *game) = 0;
+  /**
+   * @brief Handles the user input based on the state
+   *
+   * @param game Game game object containing important global informatio about
+   * the game
+   * @param event Event that has to be handeled
+   */
   virtual void handle_input(Game *game,
                             const std::optional<sf::Event> &event) = 0;
+  /**
+   * @brief Progresses the state forward
+   *
+   * @param game Game game object containing important global informatio about
+   * the game
+   * @param time The time step used to calculate the new state
+   */
   virtual void update(Game *game, float time) = 0;
+  /**
+   * @brief Renders the state objects
+   *
+   * @param game Game game object containing important global informatio about
+   * the game
+   * @param window Window for objects to be rendered in
+   */
   virtual void render(Game *game, sf::RenderWindow &window) = 0;
 
 protected:
