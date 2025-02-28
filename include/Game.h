07ed10d19e3815605092f619f3515e59ad4531ae
zak-off-zak <Zakhar_2004@inbox.ru>
2025-02-28 18:19:56 +0100

    Game class docs added

--- include/Game.h
+++ include/Game.h
@@ -6,19 +6,66 @@
 #include <memory>
 #include <stack>
 
+/**
+ * @class Game
+ * @brief Class managing all of important inforamtion about game states and
+ * transitions between them.
+ *
+ */
 class Game {
 public:
+  /**
+   * @brief Tells whether the collition has been detected
+   */
   bool game_over;
+  /**
+   * @brief Score of the player
+   */
   long long score;
+  /**
+   * @brief Number of pipes that has been poped for the pipes vector due to them
+   * being not seen on the screen (they are too far left)
+   */
   long long poped_pipes;
 
+  /**
+   * @brief Constructor of the game class
+   */
   Game();
 
+  /**
+   * @brief Pushes a new state to the top of the stack
+   *
+   * @param state State to be pushed to the top of the stack
+   */
   void push_state(std::unique_ptr<GameState> state);
+  /**
+   * @brief Pops a state from the top of the stack
+   */
   void pop_state();
+  /**
+   * @brief Popes the most recent state form the top of the stack and pushes a
+   * new one
+   *
+   * @param state State to be pushed to the top
+   */
   void changeState(std::unique_ptr<GameState> state);
+  // TODO:: Rename to peek?
+  /**
+   * @brief Returns the current state from the top of the state
+   *
+   * @return [TODO:return]
+   */
   GameState *get_current_state();
+  /**
+   * @brief Returns the window of the game
+   */
   sf::RenderWindow &get_window();
+  /**
+   * @brief Runs the game by taking a state form the top of the stack,
+   * initializing it and running the handle_input(), update() and render()
+   * methods of that state
+   */
   void run();
 
 protected:
